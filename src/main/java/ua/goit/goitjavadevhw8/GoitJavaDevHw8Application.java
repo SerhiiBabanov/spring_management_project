8f8d99b52b278fb56bd5efd1c39fdec52ee80fb3
serhiibabanov <serhii.babanov@gmail.com>
2022-11-26 12:05:20 -0500

    init commit

---
+++ src/main/java/ua/goit/goitjavadevhw8/GoitJavaDevHw8Application.java
@@ -0,0 +1,13 @@
+package ua.goit.goitjavadevhw8;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoitJavaDevHw8Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoitJavaDevHw8Application.class, args);
+    }
+
+}
---
+++ src/test/java/ua/goit/goitjavadevhw8/GoitJavaDevHw8ApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.goit.goitjavadevhw8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoitJavaDevHw8ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
