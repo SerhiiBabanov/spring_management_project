6735a44bc4472f3cb01b1e6b41a9452e7500192e
serhibabanov <QM5Hiu7dPvuRpyP>
2022-11-26 23:16:03 -0500

    delete call to repository

--- src/main/java/ua/goit/service/ProductConverter.java
+++ src/main/java/ua/goit/service/ProductConverter.java
@@ -1,19 +1,14 @@
 package ua.goit.service;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
-import ua.goit.model.dao.ProducerDAO;
 import ua.goit.model.dao.ProductDAO;
 import ua.goit.model.dto.ProductDTO;
-import ua.goit.repository.ProducerRepository;
 
 @Service
 @RequiredArgsConstructor
 public class ProductConverter {
     private final ProducerConverter producerConverter;
-    private final ProducerRepository producerRepository;
 
     public ProductDTO mapToDTO(final ProductDAO productDAO) {
         ProductDTO productDTO = new ProductDTO();
@@ -28,9 +23,7 @@ public class ProductConverter {
         ProductDAO productDAO = new ProductDAO();
         productDAO.setName(productDTO.getName());
         productDAO.setPrice(productDTO.getPrice());
-        final ProducerDAO producerDAO = producerRepository.findById(productDTO.getProducer().getId())
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "producer not found"));
-        productDAO.setProducer(producerDAO);
+        productDAO.setProducer(producerConverter.mapToDAO(productDTO.getProducer()));
         return productDAO;
     }
 }
